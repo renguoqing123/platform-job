ae5a51f3931f043bbf825f09d89f3f9b87c7441c
renguoqing <1002674966@qq.com>
2019-09-04 10:46:27 +0800

    添加注解

--- src/main/java/com/platform/job/config/WebMvcConfig.java
+++ src/main/java/com/platform/job/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.platform.job.config;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author rengq
  * @date 2019-09-04 14:57
  */
+@Component
 public class WebMvcConfig implements WebMvcConfigurer{
 	/**
      * 添加静态资源文件，外部可以直接访问地址
