27206d8ffde8b39f00cd8b7df23f3e560049ccb4
renguoqing <1002674966@qq.com>
2019-09-04 11:14:39 +0800

    static文件访问

--- src/main/java/com/platform/job/config/WebMvcConfig.java
+++ src/main/java/com/platform/job/config/WebMvcConfig.java
@@ -1,6 +1,6 @@
 package com.platform.job.config;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author rengq
  * @date 2019-09-04 14:57
  */
-@Component
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 	/**
      * 添加静态资源文件，外部可以直接访问地址
@@ -19,5 +19,6 @@ public class WebMvcConfig implements WebMvcConfigurer{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 }
