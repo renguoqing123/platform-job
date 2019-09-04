4f95575d6d8f03a8ef5c11f45fbd992b6a1c4e3d
renguoqing <1002674966@qq.com>
2019-09-04 10:30:21 +0800

    添加静态资源访问地址

---
+++ src/main/java/com/platform/job/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.platform.job.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 
+ * @author rengq
+ * @date 2019-09-04 14:57
+ */
+public class WebMvcConfig implements WebMvcConfigurer{
+	/**
+     * 添加静态资源文件，外部可以直接访问地址
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
